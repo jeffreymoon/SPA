56048cb29e9a7dc7bffa6a9922d0eabfd960bcb5
jeffreymoon <novmoon73@gmail.com>
2016-03-25 01:11:25 +0900

    spa.js setup

---
+++ js/spa.js
@@ -0,0 +1,24 @@
+/*
+ * spa.js
+ * root namespace mudule
+ * /
+
+/*jslint
+  browser : true, continue : true, devel : true,
+  indent : 2, maxerr : 50, newcpa : true,
+  nomen : true, plusplus : true, regexp : true,
+  sloppy : true, vars : false, white : true
+ */
+
+/*global $, spa */
+
+var spa = (function () {
+  var initModule = function ( $container ) {
+    $container.html(
+      '<h1 style="display:inline-block; margin:25px;">'
+      + 'hello world!'
+      + '</h1>'
+    );
+  };
+  return { initModule: initModule };
+}());
